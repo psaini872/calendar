ad36ea5b545348913d26ff8873dd219ef421fad4
Pranshu <pranshusaini872@gmail.com>
2023-06-04 16:03:58 +0530

    first commit

--- pages/index.js
+++ pages/index.js
@@ -1,118 +1,167 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import dayjs from "dayjs";
+import React, { useState } from "react";
+import { generateDate, months } from "../util/calendardata.js";
+import { GrFormNext, GrFormPrevious } from "react-icons/gr";
 
-const inter = Inter({ subsets: ['latin'] })
+const New = () => {
+  const days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  const currentDate = dayjs();
+  const [today, setToday] = useState(currentDate);
 
-export default function Home() {
+  const [checkinopen, setCheckinopen] = useState(false);
+  const [checkoutopen, setCheckoutopen] = useState(false);
+
+  const [indate, setInDate] = useState(null);
+  const [outdate, setOutDate] = useState(null);
+
+  const handledate = (date) => {
+    if (checkinopen && (!outdate || outdate >= date)) {
+      setInDate(date);
+    } else if (checkinopen && (!outdate || outdate < date)) {
+      setOutDate(date);
+      setInDate(null);
+    } else if (checkoutopen && (!indate || indate <= date)) {
+      setOutDate(date);
+    } else {
+      setInDate(date);
+      setOutDate(null);
+    }
+  };
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">pages/index.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className="flex items-start justify-center mt-16 ">
+      <div className="relative w-[75%] max-w-[400px]">
+        <div className="py-2 bg-blue-300 flex justify-evenly items-center border shadow-md rounded-lg ">
+          <div>
+            <h1
+              className={`font-bold cursor-pointer transition-all hover:text-white ${
+                checkinopen && "text-white"
+              }`}
+              onClick={() => {
+                setCheckinopen((prev) => !prev);
+                setCheckoutopen(false);
+              }}
+            >
+              Check in
+            </h1>
+            {indate ? (
+              <h1 className="font-semibold text-slate-500">
+                {indate.toDate().getDate()} {months[indate.toDate().getMonth()]}
+              </h1>
+            ) : (
+              <h1>Add Date</h1>
+            )}
+          </div>
+
+          <div>
+            <h1
+              className={`font-bold cursor-pointer transition-all hover:text-white ${
+                checkoutopen && "text-white"
+              }`}
+              onClick={() => {
+                setCheckoutopen((prev) => !prev);
+                setCheckinopen(false);
+              }}
+            >
+              Check out
+            </h1>
+            {outdate ? (
+              <h1 className="font-semibold text-slate-500">
+                {outdate.toDate().getDate()}{" "}
+                {months[outdate.toDate().getMonth()]}
+              </h1>
+            ) : (
+              <h1>Add Date</h1>
+            )}
+          </div>
         </div>
+        {(checkoutopen || checkinopen) && (
+          <div className="absolute  mt-2 w-[100%] max-w-[400px]">
+            <div className="border-2 rounded-lg shadow-md ">
+              <div className="flex justify-between items-center px-1 py-2">
+                <GrFormPrevious
+                  className=" w-5 h-5 cursor-pointer hover:scale-105 transition-all "
+                  onClick={() => {
+                    setToday(today.month(today.month() - 1));
+                  }}
+                />
+                <h1 className="font-semibold">
+                  {months[today.month()]} {today.year()}
+                </h1>
+                <GrFormNext
+                  className="w-5 h-5 cursor-pointer hover:scale-105 transition-all"
+                  onClick={() => {
+                    setToday(today.month(today.month() + 1));
+                  }}
+                />
+              </div>
+              <div className="flex h-8 justify-between px-2 items-center">
+                {days.map((day, index) => {
+                  return (
+                    <h1
+                      key={index}
+                      className="text-sm text-center place-content-center text-gray-500 select-none"
+                    >
+                      {day}
+                    </h1>
+                  );
+                })}
+              </div>
+              <div className="grid grid-cols-7">
+                {generateDate(today.month(), today.year()).map(
+                  ({ date, currentMonth, today, validdata }, index) => {
+                    return (
+                      <div
+                        key={index}
+                        className={`text-center h-14 grid place-content-center text-sm border-t `}
+                      >
+                        <h1
+                          className={`
+                            ${currentMonth ? "" : "hidden"}
+                            ${
+                              validdata
+                                ? "hover:bg-black hover:text-white"
+                                : "text-gray-300 "
+                            }
+                            ${
+                              indate &&
+                              indate.toDate().toDateString() ===
+                                date.toDate().toDateString()
+                                ? "bg-blue-800 text-white"
+                                : ""
+                            }
+                            ${
+                              outdate &&
+                              outdate.toDate().toDateString() ===
+                                date.toDate().toDateString()
+                                ? "bg-blue-800 text-white"
+                                : ""
+                            }
+                            ${
+                              indate &&
+                              outdate &&
+                              date >= indate &&
+                              date <= outdate &&
+                              "bg-blue-300"
+                            }
+                            h-10 w-10 rounded-full grid place-content-center transition-all cursor-pointer select-none
+                          `}
+                          onClick={() => {
+                            handledate(date);
+                          }}
+                        >
+                          {date.date()}
+                        </h1>
+                      </div>
+                    );
+                  }
+                )}
+              </div>
+            </div>
+          </div>
+        )}
       </div>
+    </div>
+  );
+};
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
-}
+export default New;
